960ea4bc5468db15e1e1d70eefd0a61b26511c14
pupkin2001 <131904062+pupkin2001@users.noreply.github.com>
2026-03-12 13:32:19 +0300

    Also poison animals when feeding and entities eating food themselves

--- src/main/java/pupkin/toxicologistsdelight/misc/ItemUsedEventHandler.java
+++ src/main/java/pupkin/toxicologistsdelight/misc/ItemUsedEventHandler.java
@@ -2,11 +2,13 @@ package pupkin.toxicologistsdelight.misc;
 
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.PotionItem;
 import net.minecraft.world.item.alchemy.PotionUtils;
 import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.jetbrains.annotations.NotNull;
@@ -22,7 +24,7 @@ public class ItemUsedEventHandler
 	@SubscribeEvent
 	public static void onItemUsed(LivingEntityUseItemEvent.@NotNull Finish event)
 	{
-		if (!(event.getEntity() instanceof Player player)) return;
+		LivingEntity living = event.getEntity();
 		
 		ItemStack usedItem = event.getItem();
 		
@@ -31,7 +33,7 @@ public class ItemUsedEventHandler
 		int strength = PoisonUtils.getPoisonStrength(usedItem);
 		
 		if (usedItem.isEdible()) {
-			player.addEffect(new MobEffectInstance(
+			living.addEffect(new MobEffectInstance(
 					MobEffects.POISON,
 					strength * 600,
 					strength - 1
@@ -45,7 +47,25 @@ public class ItemUsedEventHandler
 					strength - 1
 			);
 			
-			player.addEffect(poisonEffect);
+			living.addEffect(poisonEffect);
+		}
+	}
+	
+	@SubscribeEvent
+	public static void onEntityInteract(PlayerInteractEvent.EntityInteract event)
+	{
+		ItemStack stack = event.getItemStack();
+		if (!PoisonUtils.isPoisoned(stack)) return;
+		
+		if (!(event.getTarget() instanceof Animal animal)) return;
+		
+		if (animal.isFood(stack)) {
+			int strength = PoisonUtils.getPoisonStrength(stack);
+			animal.addEffect(new MobEffectInstance(
+					MobEffects.POISON,
+					strength * 600,
+					strength - 1
+			));
 		}
 	}
 }

6f72998a57066bc0a5c7ca6d6fb175791a717585
pupkin2001 <131904062+pupkin2001@users.noreply.github.com>
2026-06-21 20:49:01 +0300

    Add and disable hemotoxin vial and fluid

--- src/main/java/pupkin/toxicologistsdelight/item/ToxicologistsItems.java
+++ src/main/java/pupkin/toxicologistsdelight/item/ToxicologistsItems.java
@@ -22,7 +22,8 @@ public final class ToxicologistsItems
 	
 	ACID_VIAL = effectVial("acid_vial", ToxicologistsEffects.ACID, 1, 8),
 			POISON_VIAL = effectVial("poison_vial", () -> MobEffects.POISON, 1, 16),
-			//ROT_VIAL = effectVial("rot_vial", ToxicologistsEffects.ROT, 1, 8)
+			//ROT_VIAL = effectVial("rot_vial", ToxicologistsEffects.ROT, 1, 8),
+			//HEMOTOXIN_VIAL = effectVial("hemotoxin_vial", ToxicologistsEffects.HEMOTOXIN, 1, 8),
 	
 	CURE_VIAL = ITEMS.register("cure_vial", PoisonCureItem::new);
 	
--- src/main/java/pupkin/toxicologistsdelight/misc/ToxicologistsFluids.java
+++ src/main/java/pupkin/toxicologistsdelight/misc/ToxicologistsFluids.java
@@ -21,6 +21,7 @@ public class ToxicologistsFluids
 	
 	public static final FluidRegistryObject ACID = registerAlcoholFluid("acid", new Color(98, 25, 109, 255)),
 			//FluidRegistryObject ROT = registerAlcoholFluid("rot", new Color(75, 0, 130, 255)),
+			//FluidRegistryObject HEMOTOXIN = registerAlcoholFluid("hemotoxin", new Color(84, 140, 45, 255)),
 			CURE = registerAlcoholFluid("cure", new Color(255, 255, 255, 255));
 	
 	// Overly complex code duplicate from brewers delight. Go look there for comments on how this contraption works.
